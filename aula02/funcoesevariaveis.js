cc9dc98c53899609598c32bd6907afb4d47aeeb8
Ana-Lessa <ana.mayers@outlook.com>
2023-03-08 08:40:18 -0300

    Aula sobre JS com varíaveis e funções

---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,16 @@
+var variavel1 = "olá"
+let variavel2 = 3
+const variavel3 = 8
+const obj = {
+    nome:"Ana",
+    signo: "Virgem"
+}// Formato JSON - Javascript Notation Object
+
+console.log(variavel1, "Turminha do", variavel2, "ano")
+
+console.log(variavel2+variavel3)
+
+console.log(typeof variavel1)//verifica o tipo da variavel
+
+console.log(obj.nome, obj.signo)//para não mostrar as chaves
+console.log(typeof obj)
